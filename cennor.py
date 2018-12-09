f592986be5519bad58d924c17e4cc746f973da05
HyunByungJoon <hjun555@snu.ac.kr>
2018-12-09 16:54:25 +0900

    voting complete

--- cennor.py
+++ cennor.py
@@ -2,24 +2,39 @@ import numpy as np
 import time
 import math
 import transform
-
-PI = 3.141592653589793238462643383279502884197169
+import random
 
 
 def angle(v1, v2):
-    inner_product = round(np.dot(v1, v2), 4)
-    size = round(np.linalg.norm(v1), 4) * round(np.linalg.norm(v2), 4)
+    inner_product = np.dot(v1, v2)
+    size = np.linalg.norm(v1) * np.linalg.norm(v2)
     value = inner_product / size
-    if value > 1:
+    if value >= 1:
         return 0
-    if value < -1:
-        return PI
+    if value <= -1:
+        return math.pi
     return np.arccos(value)
 
 
+def cal_alpha(v):
+    if v[1]==0 and v[2]==0:
+        return 0 # No angle between x-y plane
+    v_innerP = np.dot([v[1], v[2]],[1,0])
+    v_size = np.linalg.norm([v[1], v[2]])
+    v_value = v_innerP / v_size
+    if v_value >= 1:
+        theta = 0
+    elif v_value <= -1:
+        theta = math.pi
+    else:
+        theta = np.arccos(v_value)
+        if v[2] < 0:
+            theta = 2*math.pi - theta
+    return theta
+
 # 1.
 start_1 = time.time()
-f = open("./data/cube", 'r') #Model
+f = open("./data/ds_model", 'r') #Model
 array = []
 while True:
     line = f.readline()
@@ -44,20 +59,19 @@ start_2 = time.time()
 Hash_table = dict()
 # print(shape[0])
 for r in range(0, shape[0]):
+    print(r)
     for i in range(0, shape[0]):
         if r != i:
             dx = np_array[i, 0] - np_array[r, 0]
             dy = np_array[i, 1] - np_array[r, 1]
             dz = np_array[i, 2] - np_array[r, 2]
             # print(np_array[r, 3:6], np_array[i, 3:6], angle(np_array[r, 3:6], np_array[i, 3:6]))
-            d = round(math.sqrt((dx*dx) + (dy*dy) + (dz*dz)), 1)
-            # print(dx, dy, dz, d)
-            a1 = int(180*angle((dx, dy, dz), np_array[r, 3:6])/PI * 10) / 10
-            a2 = int(180*angle((-dx, -dy, -dz), np_array[i, 3:6])/PI * 10) / 10
-            a3 = int(180*angle(np_array[r, 3:6], np_array[i, 3:6])/PI * 10) / 10
+            d = round(math.sqrt((dx*dx) + (dy*dy) + (dz*dz)), 4)
+            a1 = round(angle((dx, dy, dz), np_array[r, 3:6]), 2)
+            a2 = round(angle((-dx, -dy, -dz), np_array[i, 3:6]), 2)
+            a3 = round(angle(np_array[r, 3:6], np_array[i, 3:6]), 2)
 
             hash_key = d, a1, a2, a3
-
             tmp = Hash_table.get(hash_key)
             if tmp:
                 tmp.append([np_array[r], np_array[i]])
@@ -69,7 +83,7 @@ print("Step 2: {:.3f}s".format(time.time() - start_2))
 
 # 3. Find F(Mr, Mi) == F(Sr, Si)
 start_3 = time.time()
-f = open("./data/target_cube", 'r') #Scene
+f = open("./data/ds_scene", 'r') #Scene
 tg_array = []
 while True:
     line = f.readline()
@@ -85,65 +99,86 @@ while True:
 f.close()
 
 tg_np_array = np.array(tg_array)
-shape = np.shape(tg_np_array)
-r = 4
-
+tg_shape = np.shape(tg_np_array)
 voting_table = dict()
 transform_table = dict()
+r_set = set()
+#r = 4  # temporary value. should do change r repeatedly
+while True :
+    r_set.add(random.randrange(0, tg_shape[0]))  # make r_set
+    if len(r_set)==5 :
+        break
 
-for i in range(0, np.shape(tg_np_array)[0]):
-    if not i == r:
-        dx = tg_np_array[i, 0] - tg_np_array[r, 0]
-        dy = tg_np_array[i, 1] - tg_np_array[r, 1]
-        dz = tg_np_array[i, 2] - tg_np_array[r, 2]
-        d = round(math.sqrt((dx*dx) + (dy*dy) + (dz*dz)), 1)
-        a1 = int(180*angle((dx, dy, dz), tg_np_array[r, 3:6])/PI * 10) / 10
-        a2 = int(180*angle((-dx, -dy, -dz), tg_np_array[i, 3:6])/PI * 10) / 10
-        a3 = int(180*angle(tg_np_array[r, 3:6], tg_np_array[i, 3:6])/PI * 10) / 10
-        # a1 = round(180*angle((dx, dy, dz), tg_np_array[r, 3:6])/np.pi, 1)
-        # a2 = round(180*angle((-dx, -dy, -dz), tg_np_array[i, 3:6])/np.pi, 1)
-        # a3 = round(180*angle(tg_np_array[r, 3:6], tg_np_array[i, 3:6])/np.pi, 1)
+print(r_set)
 
-        # hash_key = d, a1, a2, a3
-        # if Hash_table.get(hash_key):
-        #     print(np.shape(Hash_table.get(hash_key)))
+for r in r_set :
+    for i in range(0,tg_shape[0]):
+        if r != i:
+            dx = tg_np_array[i, 0] - tg_np_array[r, 0]
+            dy = tg_np_array[i, 1] - tg_np_array[r, 1]
+            dz = tg_np_array[i, 2] - tg_np_array[r, 2]
+            d = round(math.sqrt((dx * dx) + (dy * dy) + (dz * dz)), 4)
+            a1 = round(angle((dx, dy, dz), tg_np_array[r, 3:6]), 2)
+            a2 = round(angle((-dx, -dy, -dz), tg_np_array[i, 3:6]), 2)
+            a3 = round(angle(tg_np_array[r, 3:6], tg_np_array[i, 3:6]), 2)
+            # for key in Hash_table.keys():
+            #     print(key)
+            hash_key = d, a1, a2, a3
+            item_list = Hash_table.get(hash_key)
+            #print("this is hash_key :")
+            #print(hash_key)
+            #print("item num:")
+            #print(len(item_list))
+            if item_list:
+                for item in item_list:
+                    Mr = item[0]
+                    Mi = item[1]
+                    Sr = tg_np_array[r]
+                    Si = tg_np_array[i]
+                    T_mtog = transform.transformation(Mr, [0,0,0,1,0,0])
+                    T_stog = transform.transformation(Sr, [0,0,0,1,0,0])
+                    Mvector = Mi[0:3]-Mr[0:3]
+                    Svector = Si[0:3]-Sr[0:3]
+                    Mvector_transformed = np.matmul(T_mtog, Mvector)
+                    Svector_transformed = np.matmul(T_stog, Svector)
 
-        # for key in Hash_table.keys():
-        #     print(key)
+                    alpha = cal_alpha(Svector_transformed) - cal_alpha(Mvector_transformed)
+                    if alpha < 0 :
+                        alpha = 2*math.pi + alpha
 
-        hash_key = d, a1, a2, a3
-        item_list = Hash_table.get(hash_key)
-        # print(hash_key)
-        for item in item_list:
-            #print(item)
-            Mr = item[0]
-            Mi = item[1]
-            Sr = tg_np_array[r]
-            Si = tg_np_array[i]
-            trans_matrix = transform.transformation(Sr, Mr)
+                    alpha = round(alpha, 3)
+                    #print("alph value is ", alpha)
+                    #print("S alpha is ", cal_alpha(Svector_transformed), "M alpha is ", cal_alpha(Mvector_transformed))
+                    #alpha = round(angle(vector1, vector2_trans)*180/PI, 0) # cannot make 0~2pi
+                    count = voting_table.get(alpha)
+                    trans_dict_temp = transform_table.get(alpha)
+                    if count:
+                        count += 1
+                        voting_table[alpha] = count
+                        trans_dict_temp.append([T_mtog, T_stog])
+                        transform_table[alpha] = trans_dict_temp
 
-            vector1 = Mi[0:3]-Mr[0:3]
-            vector2 = Si[0:3]-Sr[0:3]
-            vector2_trans = np.dot(trans_matrix, vector2)
-            alpha = round(angle(vector1, vector2_trans)*180/PI, 0)
+                    else:
+                        voting_table[alpha] = 1
+                        transform_table[alpha] = [T_mtog, T_stog]
 
-            count = voting_table.get(alpha)
-            trans_dict_temp = transform_table.get(alpha)
-            if count:
-                count += 1
-                voting_table[alpha] = count
-                trans_dict_temp.append(trans_matrix)
-                transform_table[alpha] = trans_dict_temp
             else:
-                voting_table[alpha] = 1
-                transform_table[alpha] = trans_matrix
+                print("No item list. hash_key : ", hash_key)
+
 
 print("Step 3: {:.3f}s".format(time.time() - start_3))
 
+vote_num = -1
+winner = 0
 
+print("key numbers : ", len(voting_table.keys()))
 for key in voting_table.keys():
-    print(key, voting_table.get(key), transform_table.get(key))
+    temp = voting_table.get(key)
+    print(key, temp)  #, transform_table.get(key)
+    if (vote_num < temp) :
+        winner = key
+        vote_num = temp
 
-
-
+print("winner is :", winner)
+print("transform matrix as a tuple : ", transform_table.get(winner))
 
--- transform.py
+++ transform.py
@@ -2,20 +2,21 @@ import numpy as np
 import math
 
 
-def transformation(mat1, mat2):
+def transformation(mat1, mat2): #need to add translation now only rotation
 	# print(mat1[3:6], mat2[3:6], np.dot(mat1[3:6], mat2[3:6]))
 	cos = np.dot(mat1[3:6], mat2[3:6])
 	sin = math.sqrt(1 - cos*cos)
 
-	a1 = mat1[3]
-	a2 = mat1[4]
-	a3 = mat1[5]
-	b1 = mat2[3]
-	b2 = mat2[4]
-	b3 = mat2[5]
-	out_prod = np.array([a2*b3 - a3*b2, a3*b1 - a1-b3, a1*b2 - a2*b1])
+	a1, a2, a3 = mat1[3], mat1[4], mat1[5]
+	b1, b2, b3 = mat2[3], mat2[4], mat2[5]
+	out_prod = np.array([a2*b3 - a3*b2, a3*b1 - a1*b3, a1*b2 - a2*b1])
+	#translate = mat2[3:6] - mat1[3:6]
 
-	v = out_prod / np.linalg.norm(out_prod)
+	if round(np.linalg.norm(out_prod),8) == 0. :
+		v = [0. ,0. ,0.]
+	else:
+		v = out_prod / np.linalg.norm(out_prod)
+
 	x = v[0]
 	y = v[1]
 	z = v[2]
@@ -23,5 +24,4 @@ def transformation(mat1, mat2):
 	trans = [[cos + (x**2 * (1-cos)), x*y*(1-cos) - z*sin, x*z*(1-cos) + y*sin],
 	         [x*y*(1-cos) + z*sin, cos + y**2*(1-cos), y*z*(1-cos) - x*sin],
 	         [x*z*(1-cos) - y*sin, y*z*(1-cos) + x*sin, cos + z**2*(1-cos)]]
-
 	return trans
